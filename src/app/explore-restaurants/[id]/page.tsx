6843dba2406c574be96a5d7fb55cf4416a76ab3c
Kongkon Jowarder <kongkon4545@gmail.com>
2024-12-10 17:53:58 +0600

    problem resolve

--- src/app/explore-restaurants/[id]/page.tsx
+++ src/app/explore-restaurants/[id]/page.tsx
@@ -1,18 +1,34 @@
-import ViewRestaurant from "@/components/ExploreRestaurants/ViewRestaurant/ViewRestaurant";
-import React from "react";
+// import ViewRestaurant from '@/components/ExploreRestaurants/ViewRestaurant/ViewRestaurant';
+// import React from 'react';
 
-interface PageProps {
-  params: {
-    id: string | number;
-  };
-}
+// interface ViewRestaurantPageProps {
+//     params: {
+//         id: number;
+//     };
+// }
 
-const Page: React.FC<PageProps> = ({ params }) => {
-  return (
-    <div>
-      <ViewRestaurant id={params?.id} />
-    </div>
-  );
+// const ViewRestaurantPage: React.FC<ViewRestaurantPageProps> = ({ params }) => {
+//     const id = params?.id;
+//     return (
+//         <div>
+//             <ViewRestaurant id={id} />
+//         </div>
+//     );
+// };
+
+// export default ViewRestaurantPage;
+
+
+
+import ViewRestaurant from '@/components/ExploreRestaurants/ViewRestaurant/ViewRestaurant';
+import React from 'react';
+
+const ViewRestaurantPage = ({params}:any) => {
+    return (
+        <div>
+            <ViewRestaurant id={params?.id} />
+        </div>
+    );
 };
 
-export default Page;
+export default ViewRestaurantPage;
